b83629828b9e035c08edab6fcb924dec8a3a116a
elias <99310710+bo-codes@users.noreply.github.com>
2022-09-01 00:37:58 -0700

    fixed home page

--- react-app/src/components/Pages/HomePage/HomePage.js
+++ react-app/src/components/Pages/HomePage/HomePage.js
@@ -1,17 +1,18 @@
-// -------- IMPORT REACT STUFF -------- //
+// IMPORT REACT STUFF --------
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { HashLink } from "react-router-hash-link";
-// -------- COMPONENTS -------- //
+import { NavLink } from "react-router-dom";
+// --------COMPONENTS -------- //
 import PostCard from "../../Posts/Elements/PostCard/PostCard";
-// --------- THUNKS -------- //
+// -------- THUNKS -------- //
 import { getAllCommentsThunk } from "../../../store/comments";
-import { getAllPostsThunk } from "../../../store/posts";
-// --------- CSS AND IMAGES -------- //
-import "./images.css";
-import sleepinCat from "../../../images/sleepin-cat.png";
+import { getAllPostsThunk, thunkGetFeedPosts } from "../../../store/posts";
+import { getAllLikes } from "../../../store/likes";
+// -------- CSS/IMAGES -------- //
+import "./Posts.css";
+import { thunkGetFollowedPosts } from "../../../store/session";
 
-function HomePage() {
+function HomePage({}) {
   const dispatch = useDispatch();
   // PULLING ALL OF THE INFORMATION FROM OUR STATE
   // THIS RUNS FIRST BEFORE USEEFFECT FETCHES OUR DATA WHICH IS WHY WE ALWAYS HAVE TO IMPLEMENT
@@ -20,104 +21,60 @@ function HomePage() {
   const comments = Object.values(useSelector((state) => state.comments));
   const user = useSelector((state) => state.session.user);
   const users = useSelector((state) => Object.values(state.user));
+  const likes = Object.values(useSelector((state) => state.likes));
 
-  const postImages = posts.filter((post) => post.image_url);
   const shuffledUsers = users.sort(() => Math.random() - 0.5);
 
   // WE ADD DISPATCH TO THE DEPENDENCY ARR SO THAT IT DOESNT RERENDER A MILLION TIMES, I JUST CANT EXPLAIN IT WELL
   useEffect(() => {
+    // console.log(likes);
     // GET ALL POSTS
-    dispatch(getAllPostsThunk());
+    dispatch(thunkGetFeedPosts(user.id));
     // GET ALL COMMENTS
     dispatch(getAllCommentsThunk());
+    // GET ALL LIKES
+    dispatch(getAllLikes());
   }, [dispatch]);
 
   return (
-    <div className="background">
-      <div className="container-titles-and-img-divs">
-        <div className="title-container">
-          <h1 className="title">
-            <span>Z</span>
-            <span className="onambulo">O N A M B U L O</span>
-          </h1>
-          <h1 className="title">
-            <span>z</span>
-            <span className="onambulo">O N A M B U L O</span>
-          </h1>
-          <h1 className="title">
-            <span>Z</span>
-            <span className="onambulo">O N A M B U L O</span>
-          </h1>
-          <h1 className="title">
-            <span>z</span>
-            <span className="onambulo">O N A M B U L O</span>
-          </h1>
-          <h1 className="title">
-            <span>z</span>
-            <span className="onambulo">O N A M B U L O</span>
-          </h1>
-          <h1 className="title">
-            <span>Z</span>
-            <span className="onambulo">O N A M B U L O</span>
-          </h1>
-          <h1 className="title">
-            <span>Z</span>
-            <span className="onambulo">O N A M B U L O</span>
-          </h1>
-          <h1 className="title">
-            <span>z</span>
-            <span className="onambulo">O N A M B U L O</span>
-          </h1>
-          <h1 className="title">
-            <span>z</span>
-            <span className="onambulo">O N A M B U L O</span>
-          </h1>
-          <h1 className="title">
-            <span>z</span>
-            <span className="onambulo">O N A M B U L O</span>
-          </h1>
+    <main>
+      <div className="post-list">
+        <div className="suggested-users">
+          {/* <h3>Suggested Users:</h3> */}
+          {/* {shuffledUsers.slice(0, 5).map((listedUser) => {
+            return (
+              <div key={listedUser.id}>
+                <NavLink to={`/${listedUser.username}`}>
+                  <img src={listedUser.profile_pic}></img>
+                  <div>{listedUser.username}</div>
+                </NavLink>
+              </div>
+            );
+          })} */}
         </div>
-        <div className="cat-image-and-title-container">
-          <div
-            className="cat-image-container"
-            style={{
-              backgroundImage: "url(" + sleepinCat + ")",
-              backgroundPosition: "center",
-              backgroundSize: "contain",
-              backgroundRepeat: "no-repeat",
-            }}
-          ></div>
-          <div className="home-subtitle-container">
-            <div className="home-subtitle">Log Your Dreams</div>
-          </div>
-        </div>
+        {/* CHECK IF THERE ARE POSTS SO THAT THE USESELECTOR DOESNT MESS US UP */}
+        {posts &&
+          posts.map((post) => {
+            // WE FILTER THROUGH ALL COMMENTS EVER TO ONLY GRAB THE ONES ASSOCIATED WITH THIS POST
+            // console.log(likes, "LIKES BEFORE EVEN FILTERING");
+            let postComments = comments.filter((comment) => {
+              return parseInt(comment.post_id) === parseInt(post.id);
+            });
+
+            // RETURNING A POST CARD WHICH IS A COMPONENT THAT DETERMINES HOW THE POST IS STRUCTURED
+            return (
+              // EACH ITEM IN A MAP NEEDS ITS OWN UNIQUE KEY
+              <a key={post.id} name={post.id} id={post.id}>
+                <PostCard
+                  post={post}
+                  postComments={postComments}
+                  likes={likes}
+                />
+              </a>
+            );
+          })}
       </div>
-      <div id="imageCard">
-        {postImages.map((image) => {
-          return (
-            <div
-              key={image.id}
-              style={{ justifyContent: "center", display: "flex" }}
-            >
-              <HashLink
-                to={`/posts#${image.id}`}
-                className="image"
-                style={{ position: "relative" }}
-              >
-                <div className="overlay">
-                  <h3>{image.caption.slice(0, 200)}</h3>
-                </div>
-                <img
-                  src={image.image_url}
-                  alt="coverImg"
-                  className="image"
-                ></img>
-              </HashLink>
-            </div>
-          );
-        })}
-      </div>
-    </div>
+    </main>
   );
 }
 
